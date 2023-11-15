bbfd00e941dd7e69eeddd389ff1e8dbb43470271
Brennez <tchalisantos40@gmail.com>
2023-11-15 14:22:16 -0300

    feat: open a external link on click

--- lib/components/text_button_component.dart
+++ lib/components/text_button_component.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TextButtonComponent extends StatelessWidget {
   const TextButtonComponent({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Future<void> _launchURL() async {
+      Uri url = Uri.parse('https://www.google.com.br/?hl=pt-BR');
+
+      if (!await launchUrl(url)) {
+        throw Exception('Não foi possível acessar: $url');
+      }
+    }
+
     return TextButton(
-      onPressed: () {},
+      onPressed: () => _launchURL(),
       child: Text(
         'Política de privacidade',
         style: TextStyle(
